046a6a36b006168dce47001a5556df6c0716f688
Vinayak Dwivedi <vinayakdwivedi@gmail.com>
2022-04-01 12:19:40 +0530

    first commit

--- src/Notfound.jsx
+++ src/Notfound.jsx
@@ -5,7 +5,7 @@ function NotFound() {
   return (
     <div className="h-screen bg-black flex flex-col justify-center items-center space-y-4">
       <h1 class="text-white text-6xl">ERROR - 404</h1>
-      <h1 className="text-7xl text-white">Page Cannot be found</h1>
+      <h2 className="text-7xl text-white">Page Cannot be found</h2>
       <Link to="/lectures" class="text-blue-300 font-medium text-2xl mt-4">
         Go back to lectures
       </Link>
